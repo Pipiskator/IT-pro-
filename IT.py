dff8896ee77b5d3fac02e033e83e948207a20472
PExaB <120641343+PExaB@users.noreply.github.com>
2022-12-26 21:08:13 +0800

    Update IT.py

--- IT.py
+++ IT.py
@@ -4,19 +4,10 @@ import os
 import time
 
 
-def add_watermark_imp(image, watermark, opacity = 1, wm_interval = 0): # создание водяного знака
-    assert opacity >= 0 and opacity <= 1
-    if opacity < 1: #Проверка режима изображения
-        if watermark.mode != 'RGBA':
-            watermark = watermark.convert('RGBA')
-        else:
-            watermark = watermark.copy()
-        alpha = watermark.split()[3]
-        alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
-        watermark.putalpha(alpha)
-        
+def add_watermark_imp(image, watermark): # создание водяного знака
     layer = Image.new('RGBA', image.size, (0,0,0,0)) #координаты  лого
-    layer.paste(watermark, (0, 0))
+    
+    layer.paste(watermark, (0, 0)) #расположение
     return Image.composite(layer,  image,  layer)
 
 
@@ -26,13 +17,13 @@ def add_watermark(image_path, watermark_path):
     
     length = watermark.size[0] 
     width = watermark.size[1] 
-    watermark = watermark.resize((int(length / 16), int(width / 9)), Image.ANTIALIAS) #задаем размер логопо размеру
     
-    result = add_watermark_imp(img,watermark)
+    watermark = watermark.resize((int(length / 16), int(width / 9)), Image.ANTIALIAS) #задаем размер логопо размеру
+    result = add_watermark_imp(img, watermark)
+    
     new_path = image_path + '_' + '.jpg' #даем новое имя готовому фото
     result.save(new_path) #сохраняем готовое фото
     return new_path
-
 
 def clear_content(chat_id): #отчистка 
     try:
@@ -65,32 +56,31 @@ def go(message):
 @bot.message_handler(content_types=['photo']) #обоработка фота
 def handle_docs_photo(message):
     print(message.photo[:-2])
-    
     images[str(message.chat.id)] = [] #заполнение словаря
     try:
         file_info = bot.get_file(message.photo[len(message.photo)-1].file_id) #id последней картинки
-        
         downloaded_file = bot.download_file(file_info.file_path) 
         source = 'tmp/' + file_info.file_path 
 
+        
         with open(source, 'wb') as new_file:
            new_file.write(downloaded_file)
-           
+        
+        
         bot.reply_to(message,"Фото добавлено") #ответ на фотографии пользователю
         images[str(message.chat.id)].append(source)
     except Exception as error: 
         bot.reply_to(message, error ) #сообщение об ошибки
 
+        
     time.sleep(3) #ждем 3 сек, ждем второе фото
     print('img: ', images)
-    reply_img = ''  
-    
+    reply_img = ''    
     if (len(images[str(message.chat.id)]) == 2): #если бот увидел две фотографии
-        
         reply_img = add_watermark(images[str(message.chat.id)][1], images[str(message.chat.id)][0]) #добавляем водяной знак
-        
         images[str(message.chat.id)].append(reply_img) #отправка пользователю
         bot.send_photo(message.chat.id, open(reply_img, 'rb'))
         clear_content(str(message.chat.id))
-        
+    
+    
 bot.polling(none_stop=True, interval=0)   
